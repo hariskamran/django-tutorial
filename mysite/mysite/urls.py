d35b86f8744b7939f53487c604d24233b6aa9a7f
Haris Kamran <haris@meistery.net>
2023-10-03 14:22:15 +0500

    Static files fix.

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,5 +20,3 @@ urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
